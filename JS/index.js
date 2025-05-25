2d13ecfdb15e0eda94ea64b42572384957caacfe
FeX1EE <95926554+FeX1EE@users.noreply.github.com>
2025-05-25 17:34:21 +0300

    LCM2

--- JS/index.js
+++ JS/index.js
@@ -37,25 +37,60 @@ gsap.utils.toArray("section").forEach(section => {
     )
 })
 function initPlayer(){
+    const musicData=[["Mus\\Gaijin_Entertainment_-_Arise_Great_Country_48152311.mp3","Gay jin","Arise Great Country"],["Mus\\Gaijin_Entertainment_-_Red-Starred_Avengers_(musmore.org).mp3","Gay jin","Red-Starred Avengers"]]
     const btn = document.querySelector(".playButton")
-    const audio = document.getElementsByClassName(".audioPlayer")
-    document.addEventListener("click",() => {
+    const audio = document.querySelector(".audioPlayer")
+    const plrIc = document.querySelectorAll(".plrIc")
+    const musProg = document.querySelector(".musProg")
+    const prog = document.querySelector(".Prog")
+    const name = document.querySelector(".musName")
+    const prod = document.querySelector(".musPrd")
+    const time = document.querySelector(".musTime")
+    let rand = Math.floor(Math.random()*2)
+    console.log()
+    audio.src=musicData[rand][0]
+    prod.innerHTML=musicData[rand][1]
+    name.innerHTML=musicData[rand][2]
+    btn.addEventListener("click",() => {
         if (audio.paused){
             audio.play()
-            // btn.innerHTML=""
+            plrIc[0].classList.replace("triangle","rect")
+            plrIc[1].classList.add("rect")
         }
         else{
             audio.pause()
-            // btn.innerHTML=""
+            plrIc[0].classList.replace("rect","triangle")
+            plrIc[1].classList.remove("rect")
         }
     })
-    const time = document.querySelector(".musTime")
-    let Mmins = Math.floor(audio.duration/60)
-    let Msecs = Math.floor(audio.duration%60).toString().padStart(2,'0')
+    let Mmins
+    let Msecs
+    audio.addEventListener("loadeddata",() => {
+        Mmins = Math.floor(audio.duration/60)
+        Msecs = Math.floor(audio.duration%60).toString().padStart(2,'0')
+    })
     audio.addEventListener("timeupdate",() => {
+        prog.style.width=`${audio.currentTime/audio.duration*99+1}%`
         let mins = Math.floor(audio.currentTime/60)
         let secs = Math.floor(audio.currentTime%60).toString().padStart(2,'0')
         time.innerHTML = `${mins}:${secs} / ${Mmins}:${Msecs}`
+    })
+    let progHold=false
+    musProg.addEventListener("mousedown", (e) => {
+        audio.pause()
+        plrIc[0].classList.replace("rect","triangle")
+        plrIc[1].classList.remove("rect")
+        progHold=true
+    })
+    document.addEventListener("mouseup", (e) => {
+        progHold=false
+    })
+    document.addEventListener("mousemove", (e) => {
+        if (progHold){
+            const rect = musProg.getBoundingClientRect()
+            const pos = (e.clientX-rect.left)/rect.width
+            audio.currentTime=pos*audio.duration
+        }
     })
 }
 document.addEventListener("DOMContentLoaded", initPlayer)
